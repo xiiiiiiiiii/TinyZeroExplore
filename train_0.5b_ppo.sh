c8a417a7ce866896c1d6f5cea0a76d6fb63c2c9c
Qilong Wu <jerrywu27705751@gmail.com>
2025-01-27 21:42:22 +0800

    Adapt to own scripts and config for A100.

---
+++ train_0.5b_ppo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# alias python='/home/weiji/anaconda3/envs/zero/bin/python'
+# alias python3='/home/weiji/anaconda3/envs/zero/bin/python3'
+# alias pip='/home/weiji/anaconda3/envs/zero/bin/pip'
+
+export N_GPUS=1
+export CUDA_VISIBLE_DEVICES=5
+ray stop --force && ray start --head --include-dashboard=True
+export BASE_MODEL="model/Qwen2.5-0.5B"
+export DATA_DIR="data/countdown"
+export ROLLOUT_TP_SIZE=1
+export EXPERIMENT_NAME=countdown-qwen2.5-0.5b
+export VLLM_ATTENTION_BACKEND=XFORMERS
+
+bash ./scripts/train_tiny_zero.sh
---
+++ train_3b_ppo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# alias python='/home/weiji/anaconda3/envs/zero/bin/python'
+# alias python3='/home/weiji/anaconda3/envs/zero/bin/python3'
+# alias pip='/home/weiji/anaconda3/envs/zero/bin/pip'
+
+export N_GPUS=2
+export CUDA_VISIBLE_DEVICES=2,3
+ray stop --force && ray start --head --include-dashboard=True
+export BASE_MODEL="model/Qwen2.5-3B"
+export DATA_DIR="data/countdown"
+export ROLLOUT_TP_SIZE=2
+export EXPERIMENT_NAME=countdown-qwen2.5-3b
+export VLLM_ATTENTION_BACKEND=XFORMERS
+
+bash ./scripts/train_tiny_zero_a100.sh
