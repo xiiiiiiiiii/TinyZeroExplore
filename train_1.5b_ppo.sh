8326fbbe6b7f1c62b4ab52e0e1d7f170e10a812d
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-02-26 23:15:39 -0800

    Fixes

--- train_1.5b_ppo.sh
+++ train_1.5b_ppo.sh
@@ -6,14 +6,14 @@
 ray stop --force && ray start --head --num-gpus=1 --port=6379
 
 # On worker node.
-#ray stop --force && ray start --num-gpus=1 --address='[driver_ip]:6379' --block
+# ray stop --force && ray start --num-gpus=1 --address='[driver_ip]:6379' --block
+# ray stop --force && ray start --num-gpus=1 --address='10.19.17.144:6379' --block
 
-export RAY_ADDRESS=[driver_ip]:6379
-
+export RAY_ADDRESS=10.19.17.144:6379
 export N_GPUS=2
 export CUDA_VISIBLE_DEVICES=2,3
-export BASE_MODEL="model/Qwen2.5-1.5B"
-export DATA_DIR="data/countdown"
+export BASE_MODEL=model/Qwen2.5-1.5B
+export DATA_DIR=data/countdown
 export ROLLOUT_TP_SIZE=2
 export EXPERIMENT_NAME=countdown-qwen2.5-1.5b
 export VLLM_ATTENTION_BACKEND=XFORMERS
