0741573268424a0bd5cf57e2dfdf21bbc7e4a0d7
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-02-27 01:07:13 -0800

    Fixes

--- train_0.5b_ppo.sh
+++ train_0.5b_ppo.sh
@@ -3,11 +3,12 @@
 # alias python3='/home/weiji/anaconda3/envs/zero/bin/python3'
 # alias pip='/home/weiji/anaconda3/envs/zero/bin/pip'
 
+ray stop --force && ray start --head # --include-dashboard=True
+
 export N_GPUS=1
 export CUDA_VISIBLE_DEVICES=2
-ray stop --force && ray start --head --include-dashboard=True
-export BASE_MODEL="model/Qwen2.5-0.5B"
-export DATA_DIR="data/countdown"
+export BASE_MODEL=Qwen/Qwen2.5-1.5B
+export DATA_DIR=data/countdown
 export ROLLOUT_TP_SIZE=1
 export EXPERIMENT_NAME=countdown-qwen2.5-0.5b
 export VLLM_ATTENTION_BACKEND=XFORMERS
--- train_1.5b_ppo.sh
+++ train_1.5b_ppo.sh
@@ -3,13 +3,8 @@
 # alias python3='/home/weiji/anaconda3/envs/zero/bin/python3'
 # alias pip='/home/weiji/anaconda3/envs/zero/bin/pip'
 
-ray stop --force && ray start --head --num-gpus=1 --port=6379
+ray stop --force && ray start --head --num-gpus=2 --port=6379
 
-# On worker node.
-# ray stop --force && ray start --num-gpus=1 --address='[driver_ip]:6379' --block
-# ray stop --force && ray start --num-gpus=1 --address='10.19.17.144:6379' --block
-
-export RAY_ADDRESS=10.19.17.144:6379
 export N_GPUS=2
 export CUDA_VISIBLE_DEVICES=2,3
 export BASE_MODEL=Qwen/Qwen2.5-1.5B
