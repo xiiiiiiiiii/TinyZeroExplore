3782f76a5b504ea9a9236502de9fb8ac55c7be05
xiiiiiiiiii <xiiiiiiiiii@gmail.com>
2025-02-27 20:44:02 -0800

    Add 1.5b grpo

---
+++ train_1.5b_grpo.sh
@@ -0,0 +1,12 @@
+export N_GPUS=1
+export CUDA_VISIBLE_DEVICES=0
+
+ray stop --force && ray start --head
+
+export BASE_MODEL=Qwen/Qwen2.5-1.5B
+export DATA_DIR=data/countdown
+export ROLLOUT_TP_SIZE=1
+export EXPERIMENT_NAME=countdown-qwen2.5-1.5b
+export VLLM_ATTENTION_BACKEND=XFORMERS
+
+bash ./scripts/train_tiny_zero_a100_grpo.sh
